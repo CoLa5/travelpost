3fefbaba7c766ca089b2f4fd7e94379b633792ae
Corvin Lasogga <c.lasogga@gmail.com>
2025-11-24 17:40:20 +0100

    feat(readers): Add imports to init to always support implemented readers

--- src/travelpost/readers/__init__.py
+++ src/travelpost/readers/__init__.py
@@ -1,3 +1,17 @@
+"""Readers."""
+
+# ruff: noqa: F401
+
 from pillow_heif import register_heif_opener
 
+from travelpost.readers.directory import DirectoryReader
+from travelpost.readers.file import FileReader
+from travelpost.readers.file import supported_formats
+import travelpost.readers.heic
+import travelpost.readers.mov
+
 register_heif_opener()
+
+FORMATS: set[str] = supported_formats()
+
+__all__ = ("DirectoryReader", "FileReader", "FORMATS")
