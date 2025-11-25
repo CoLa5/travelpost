bd0e17b400249b833842bb54788d6d3e0a735cad
Corvin Lasogga <c.lasogga@gmail.com>
2025-11-25 15:13:25 +0100

    fix(readers): Add missing import of MediaType

--- src/travelpost/readers/interface/__init__.py
+++ src/travelpost/readers/interface/__init__.py
@@ -2,5 +2,6 @@
 
 from travelpost.readers.interface.abc import MediaReaderABC
 from travelpost.readers.interface.md import MediaMetadata
+from travelpost.readers.interface.md import MediaType
 
-__all__ = ("MediaReaderABC", "MediaMetadata")
+__all__ = ("MediaReaderABC", "MediaMetadata", "MediaType")
