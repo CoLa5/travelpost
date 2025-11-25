2ff70037eb6128742da871d3cbff4937e427fdba
Corvin Lasogga <c.lasogga@gmail.com>
2025-11-25 22:10:46 +0100

    refactor(readers): Made interface a file (formerly subpackage)

--- src/travelpost/readers/photo_video/interface/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Interface."""
-
-from readers.abc import MediaReaderABC
-
-from travelpost.readers.photo_video.interface.md import MediaMetadata
-from travelpost.readers.photo_video.interface.md import MediaType
-
-__all__ = ("MediaReaderABC", "MediaMetadata", "MediaType")
