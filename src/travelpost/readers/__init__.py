f31ac4d1d953cce1335f76f10b9dfd03136a47b4
Corvin Lasogga <c.lasogga@gmail.com>
2025-11-25 21:32:12 +0100

    feat(readers): add excel reader

--- src/travelpost/readers/__init__.py
+++ src/travelpost/readers/__init__.py
@@ -5,6 +5,7 @@
 from pillow_heif import register_heif_opener
 
 from travelpost.readers.directory import DirectoryReader
+import travelpost.readers.excel
 from travelpost.readers.file import FileReader
 from travelpost.readers.file import supported_formats
 import travelpost.readers.heic
---
+++ src/travelpost/readers/excel.py
@@ -0,0 +1,36 @@
+"""Excel Reader."""
+
+import datetime
+
+import geopandas as gpd
+import pandas as pd
+import shapely
+
+from travelpost.readers.file import register_reader
+from travelpost.readers.interface import MediaReaderABC
+
+
+class ExcelReader(MediaReaderABC):
+    def read(self) -> pd.DataFrame:
+        df = pd.read_excel(self._path)
+        df["location"] = [
+            shapely.Point(x, y, z)
+            for x, y, z in zip(
+                df.longitude, df.latitude, df.altitude, strict=True
+            )
+        ]
+        t_off = df.timestamp - df.timestamp_utc
+        df["timestamp"] = [
+            t.replace(tzinfo=datetime.timezone(o))
+            for t, o in zip(df.timestamp, t_off, strict=True)
+        ]
+        df = df.drop(
+            columns=["longitude", "latitude", "altitude", "timestamp_utc"]
+        )
+        return gpd.GeoDataFrame(df, geometry="location", crs="EPSG:4326")
+
+
+register_reader("xlsx", ExcelReader)
+
+if __name__ == "__main__":
+    print(ExcelReader("data/test/result.xlsx").read())
