11242eec3616b8c27f0dcf289aa7827fb985f2a9
Corvin Lasogga <c.lasogga@gmail.com>
2026-01-14 15:47:01 +0100

    feat(readers): update __init__ of utils to include NOT_SET and requests

--- src/travelpost/readers/fp/utils/__init__.py
+++ src/travelpost/readers/fp/utils/__init__.py
@@ -1,5 +1,11 @@
 """Utils."""
 
-from travelpost.readers.fp.utils.url import URL
+from travelpost.readers.fp.utils import url
+from travelpost.readers.fp.utils.not_set import NOT_SET
+from travelpost.readers.fp.utils.requests import requests
 
-__all__ = ("URL",)
+__all__ = (
+    "NOT_SET",
+    "requests",
+    "url",
+)
