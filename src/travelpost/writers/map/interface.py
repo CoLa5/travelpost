ff9deb11c08bb0a92704ac91c014c82b73a98a8d
Corvin Lasogga <c.lasogga@gmail.com>
2026-01-15 18:21:31 +0100

    feat(writers/map): Add interface

---
+++ src/travelpost/writers/map/interface.py
@@ -0,0 +1,51 @@
+"""Interface."""
+
+import dataclasses
+import pathlib
+
+
+@dataclasses.dataclass(frozen=True, kw_only=True)
+class Bounds:
+    """Bounds."""
+
+    min_latitude: float
+    min_longitude: float
+
+    max_latitude: float
+    max_longitude: float
+
+    def to_tuple(self) -> tuple[tuple[float, float], tuple[float, float]]:
+        """Returns `((lat_min, lon_min), (lat_max, lon_max))`."""
+        return (
+            (self.min_latitude, self.min_longitude),
+            (self.max_latitude, self.max_longitude),
+        )
+
+
+@dataclasses.dataclass(frozen=True, kw_only=True)
+class Point:
+    """Point."""
+
+    latitude: float
+    longitude: float
+    transport: str
+
+    @property
+    def lat_lon(self) -> tuple[float, float]:
+        """Returns `(lat, lon)`."""
+        return (self.latitude, self.longitude)
+
+
+@dataclasses.dataclass(frozen=True, kw_only=True)
+class Post:
+    """Post."""
+
+    name: str
+    latitude: float
+    longitude: float
+    image_path: pathlib.Path | None
+
+    @property
+    def lat_lon(self) -> tuple[float, float]:
+        """Returns `(lat, lon)`."""
+        return (self.latitude, self.longitude)
