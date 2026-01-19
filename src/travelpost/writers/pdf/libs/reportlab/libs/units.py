84e66b0ef05d3dd622a4268e528896ae9dbf4b84
Corvin Lasogga <c.lasogga@gmail.com>
2026-01-19 10:45:13 +0100

    feat(writers/pdf): Extend reportlab units with "pt" as explicit unit

---
+++ src/travelpost/writers/pdf/libs/reportlab/libs/units.py
@@ -0,0 +1,10 @@
+"""Units."""
+
+from reportlab.lib.units import cm
+from reportlab.lib.units import inch
+from reportlab.lib.units import mm
+from reportlab.lib.units import pica
+
+pt: float = 1.0
+
+__all__ = ("cm", "inch", "mm", "pica", "pt")
