b060b3d21ebc170ffde1a073fc1e80181d2dfd5b
Corvin Lasogga <c.lasogga@gmail.com>
2026-01-21 08:47:32 +0100

    feat(writers/pdf): Add toc entry flowable

---
+++ src/travelpost/writers/pdf/libs/reportlab/platypus/toc_entry.py
@@ -0,0 +1,65 @@
+"""Table of Contents (TOC) Entry."""
+
+from reportlab.lib.sequencer import Sequencer
+
+from travelpost.writers.pdf.libs.reportlab.platypus import Flowable
+
+
+class TOCEntry(Flowable):
+    """Table of Contents (TOC) Entry."""
+
+    _SEQ: Sequencer = Sequencer()
+    _ZEROSIZE: int = 1
+    _SPACETRANSFER: int = 1
+
+    def __init__(
+        self,
+        title: str,
+        key_prefix: str,
+        closed_outline: bool = True,
+        outline_entry: bool = True,
+        toc_entry: bool = True,
+        level: int = 0,
+    ) -> None:
+        """Initializes the Table of Contents (TOC) Entry.
+
+        Args:
+            title: The title to show in the TOC.
+            key_prefix: The key prefix to use in the PDF.
+            closed_outline: Whether to close the entry in the outline. If
+                `True`, subentries are hidden behind `-` in the outline.
+                Defaults to ``True``.
+            outline_entry: Whether to show the entry in the outline. Defaults to
+                ``True``.
+            toc_entry: Whether to show the entry in the TOC pages. Defaults to
+                ``True``.
+            level: The TOC level. Defaults to ``0``.
+        """
+        self._title = title
+        key_prefix = key_prefix.lower()
+        self._key = f"{key_prefix:s}{self._SEQ.nextf(key_prefix):s}"
+        self._closed = closed_outline
+        self._outline_entry = outline_entry
+        self._toc_entry = toc_entry
+        self._level = level
+        super().__init__(0.0, 0.0)
+
+    def draw(self) -> None:
+        self.canv.bookmarkPage(self._key)
+        if self._outline_entry:
+            self.canv.addOutlineEntry(
+                self._title,
+                self._key,
+                closed=int(self._closed),
+                level=self._level,
+            )
+        if self._toc_entry:
+            self._doctemplateAttr("notify")(
+                "TOCEntry",
+                (
+                    self._level,
+                    self._title,
+                    self.canv.getPageNumber(),
+                    self._key,
+                ),
+            )
