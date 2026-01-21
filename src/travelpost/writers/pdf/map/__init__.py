cea72b303d3f5991972f8995a29ea796af000b33
Corvin Lasogga <c.lasogga@gmail.com>
2026-01-21 11:03:12 +0100

    feat(writers/pdf): Add map flowable and page template

---
+++ src/travelpost/writers/pdf/map/__init__.py
@@ -0,0 +1,6 @@
+"""Map."""
+
+from travelpost.writers.pdf.map.page_templates import MapPage
+from travelpost.writers.pdf.map.story import map_flowables
+
+__all__ = ("MapPage", "map_flowables")
---
+++ src/travelpost/writers/pdf/map/story.py
@@ -0,0 +1,28 @@
+"""Map - Story."""
+
+import pathlib
+
+from reportlab.platypus import DocIf
+from reportlab.platypus import Flowable
+from reportlab.platypus import FrameBreak
+from reportlab.platypus import NextPageTemplate
+from reportlab.platypus import PageBreak
+
+from travelpost.writers.pdf.blank import blank_flowables
+from travelpost.writers.pdf.libs.reportlab.platypus import ImageFlowable
+from travelpost.writers.pdf.libs.reportlab.platypus.toc_entry import TOCEntry
+from travelpost.writers.pdf.map.page_templates import MapPage
+
+
+def map_flowables(
+    map_path: pathlib.Path,
+    title: str = "Map",
+) -> tuple[Flowable]:
+    return (
+        NextPageTemplate(MapPage.id),
+        PageBreak(),
+        FrameBreak(ix=MapPage.map_frame_id),
+        ImageFlowable(map_path, fit="cover"),
+        TOCEntry(title, "map", outline_entry=True, toc_entry=True),
+        DocIf("doc.page % 2 == 0", blank_flowables()),
+    )
