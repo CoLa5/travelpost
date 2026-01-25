c9ee4e737ecc6010d22e1043e3a6c9c151e4c823
Corvin Lasogga <c.lasogga@gmail.com>
2026-01-25 11:39:37 +0100

    feat(writers/pdf): Add reportlab libs init

---
+++ src/travelpost/writers/pdf/libs/reportlab/libs/__init__.py
@@ -0,0 +1,41 @@
+"""Libs."""
+
+from travelpost.writers.pdf.libs.reportlab.libs import units
+from travelpost.writers.pdf.libs.reportlab.libs.color import (
+    change_color_transparency,
+)
+from travelpost.writers.pdf.libs.reportlab.libs.color import css_color
+from travelpost.writers.pdf.libs.reportlab.libs.color import register_color
+from travelpost.writers.pdf.libs.reportlab.libs.color import to_color
+from travelpost.writers.pdf.libs.reportlab.libs.drawing import LineCap
+from travelpost.writers.pdf.libs.reportlab.libs.drawing import LineJoin
+from travelpost.writers.pdf.libs.reportlab.libs.drawing import (
+    update_drawing_attributes,
+)
+from travelpost.writers.pdf.libs.reportlab.libs.enums import TextAlignment
+from travelpost.writers.pdf.libs.reportlab.libs.enums import TextTransform
+from travelpost.writers.pdf.libs.reportlab.libs.geom_tuples import Box
+from travelpost.writers.pdf.libs.reportlab.libs.geom_tuples import Gap
+from travelpost.writers.pdf.libs.reportlab.libs.geom_tuples import Margin
+from travelpost.writers.pdf.libs.reportlab.libs.geom_tuples import Padding
+from travelpost.writers.pdf.libs.reportlab.libs.image import read_jpeg_info
+from travelpost.writers.pdf.libs.reportlab.libs.stylesheet import StyleSheet
+
+__all__ = (
+    "Box",
+    "Gap",
+    "LineCap",
+    "LineJoin",
+    "Margin",
+    "Padding",
+    "StyleSheet",
+    "TextAlignment",
+    "TextTransform",
+    "change_color_transparency",
+    "css_color",
+    "read_jpeg_info",
+    "register_color",
+    "to_color",
+    "units",
+    "update_drawing_attributes",
+)
