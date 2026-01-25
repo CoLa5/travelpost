59859b187cf6479d9e38fd5f5f0c6240a0521058
Corvin Lasogga <c.lasogga@gmail.com>
2026-01-25 11:59:22 +0100

    feat(writers/pdf): Add summary page template

---
+++ src/travelpost/writers/pdf/summary/page_templates.py
@@ -0,0 +1,36 @@
+"""Summary - Page Template."""
+
+from typing import ClassVar
+
+from travelpost.writers.pdf.flowables.paragraphs import H1
+from travelpost.writers.pdf.libs.reportlab.platypus import Frame
+from travelpost.writers.pdf.libs.reportlab.platypus import PageGapTemplateABC
+
+
+class SummaryPage(PageGapTemplateABC):
+    """Summary Page (single frame)."""
+
+    id: ClassVar[str] = "summary"
+    title_frame_id: ClassVar[str] = "title_frame"
+    body_frame_id: ClassVar[str] = "main_frame"
+
+    def _create_frames(self) -> list[Frame]:
+        body_height = (
+            self.content_height - H1.STYLE.leading - H1.STYLE.spaceAfter
+        )
+        return [
+            Frame(
+                self.title_frame_id,
+                self.margin.left,
+                self.height - self.margin.top - H1.STYLE.leading,
+                self.content_width,
+                H1.STYLE.leading,
+            ),
+            Frame(
+                self.body_frame_id,
+                self.margin.left,
+                self.margin.bottom,
+                self.content_width,
+                body_height,
+            ),
+        ]
