68e165ff7e50c38afccff498438d72e64219703f
Corvin Lasogga <c.lasogga@gmail.com>
2026-01-25 11:46:36 +0100

    refactor(writers/pdf): Minor code change of front_cover __init__

--- src/travelpost/writers/pdf/front_cover/__init__.py
+++ src/travelpost/writers/pdf/front_cover/__init__.py
@@ -3,7 +3,4 @@
 from travelpost.writers.pdf.front_cover.page_templates import FrontCoverPage
 from travelpost.writers.pdf.front_cover.story import front_cover_flowables
 
-__all__ = (
-    "FrontCoverPage",
-    "front_cover_flowables",
-)
+__all__ = ("FrontCoverPage", "front_cover_flowables")
