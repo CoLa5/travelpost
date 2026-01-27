859c3fea71114421be4919a528b544e77811e768
Corvin Lasogga <c.lasogga@gmail.com>
2026-01-27 19:05:46 +0100

    feat(writers/pdf): Add header to and remove special stats frame from post page

--- src/travelpost/writers/pdf/post/page_templates/text_image.py
+++ src/travelpost/writers/pdf/post/page_templates/text_image.py
@@ -3,24 +3,22 @@
 from typing import ClassVar
 
 from travelpost.writers.pdf.header_footer import FooterMixin
+from travelpost.writers.pdf.header_footer import HeaderMixin
 from travelpost.writers.pdf.libs.reportlab.platypus import Frame
 from travelpost.writers.pdf.libs.reportlab.platypus import PageGapTemplateABC
-from travelpost.writers.pdf.post.flowables import PostStats
 
 
-class PostStartTextPage(PageGapTemplateABC, FooterMixin):
+class PostStartTextPage(PageGapTemplateABC, HeaderMixin, FooterMixin):
     """Post - Start Text Page."""
 
     id: ClassVar[str] = "post_start_text_page"
 
-    stats_frame_id: ClassVar[str] = "stats_frame"
     left_text_frame_id: ClassVar[str] = "left_text_frame"
     right_text_frame_id: ClassVar[str] = "right_text_frame"
 
     def _create_frames(self) -> list[Frame]:
         w = (self.content_width - self.gap.column) / 2
-        h0 = self.content_height - PostStats.height - self.gap.row
-        h1 = self.content_height
+        h = self.content_height
         x0 = self.margin.left
         x1 = x0 + w + self.gap.column
 
@@ -28,22 +26,15 @@ class PostStartTextPage(PageGapTemplateABC, FooterMixin):
             Frame(
                 self.left_text_frame_id,
                 x0,
-                self.margin.bottom + PostStats.height + self.gap.row,
-                w,
-                h0,
-            ),
-            Frame(
-                self.stats_frame_id,
-                x0,
                 self.margin.bottom,
                 w,
-                PostStats.height,
+                h,
             ),
             Frame(
                 self.right_text_frame_id,
                 x1,
                 self.margin.bottom,
                 w,
-                h1,
+                h,
             ),
         ]
