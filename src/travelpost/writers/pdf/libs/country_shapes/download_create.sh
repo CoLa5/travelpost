f1730b850f67174ddd36f442c8f8a1bbca248803
Corvin Lasogga <c.lasogga@gmail.com>
2026-02-02 18:53:05 +0100

    fix(writers/pdf): Fix download script of country_shapes to verify by sha256

--- src/travelpost/writers/pdf/libs/country_shapes/download_create.sh
+++ src/travelpost/writers/pdf/libs/country_shapes/download_create.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env sh
 set -e
 
+SCALE=10m
+THEME="cultural"
 VERSION="5.1.2"
-URL="https://naciscdn.org/naturalearth/${VERSION}/10m/cultural/ne_10m_admin_0_countries.zip"
+FILE="ne_${SCALE}_admin_0_countries"
+ZIP="${FILE}.zip"
+URL="https://naciscdn.org/naturalearth/${VERSION}/${SCALE}/${THEME}/${ZIP}"
+SHA256="ce1ac7036499a0edd641fbc093cd209a98f96a49d2eca8480aaacad35138a7f6"
 TARGET_DIR="lib/natural_earth_data"
 
 TMP="$(mktemp -d)"
@@ -10,14 +15,30 @@ trap 'rm -rf "$TMP"' EXIT
 
 mkdir -p "$TARGET_DIR"
 
-curl -L "$URL" -o "$TMP/ne.zip"
-unzip -q "$TMP/ne.zip" -d "$TMP/ne"
+echo "Downloading $ZIP ..."
+curl -L "$URL" -o "$TMP/$ZIP"
 
+echo "Verifying hash ..."
+ACTUAL_SHA256=$(sha256sum "$TMP/$ZIP" | awk '{print $1}')
+if [ "$ACTUAL_SHA256" != "$SHA256" ]; then
+    echo "Hash mismatch, file may be corrupted!"
+    exit 1
+fi
+echo "Hash verified successfully."
+
+echo "Unzipping $ZIP ..."
+unzip -q "$TMP/$ZIP" -d "$TMP/$FILE"
+
+echo "Copying files ..."
+cp -R "$TMP/$FILE/." "$TARGET_DIR/"
+
+echo "Creating SVGs ..."
 SCRIPT_DIR="$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)"
 uv run python "$SCRIPT_DIR/creator/__main__.py" \
   --export-all \
   --height=600 \
   --padding=16 \
   --oversampling=4 \
-  --shp-path="$TMP/ne/ne_10m_admin_0_countries.shp" \
+  --shp-path="$TARGET_DIR/$FILE.shp" \
   --out="$TARGET_DIR"
+echo "Created SVGs."
