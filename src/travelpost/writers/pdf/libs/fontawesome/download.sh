1ff948bb9fb535c459632897f05f0d026838800d
Corvin Lasogga <c.lasogga@gmail.com>
2026-02-02 19:05:53 +0100

    fix(writers/pdf): Fix download script of fontawesome to verify by sha256

--- src/travelpost/writers/pdf/libs/fontawesome/download.sh
+++ src/travelpost/writers/pdf/libs/fontawesome/download.sh
@@ -2,7 +2,10 @@
 set -e
 
 VERSION="7.1.0"
-URL="https://use.fontawesome.com/releases/v${VERSION}/fontawesome-free-${VERSION}-desktop.zip"
+FILE="fontawesome-free-${VERSION}-desktop"
+ZIP="${FILE}.zip"
+URL="https://use.fontawesome.com/releases/v${VERSION}/${ZIP}"
+SHA256="42eb48ede199f7a9e4944398b6a4acf07e37897d99a634021e17574ad83590cf"
 TARGET_DIR="lib/fontawesome"
 
 TMP="$(mktemp -d)"
@@ -10,7 +13,21 @@ trap 'rm -rf "$TMP"' EXIT
 
 mkdir -p "$TARGET_DIR"
 
-curl -L "$URL" -o "$TMP/fa.zip"
-unzip -q "$TMP/fa.zip" -d "$TMP"
+echo "Downloading $ZIP ..."
+curl -L "$URL" -o "$TMP/$ZIP"
 
-cp -R "$TMP/fontawesome-free-${VERSION}-desktop/." "$TARGET_DIR/"
+echo "Verifying hash ..."
+ACTUAL_SHA256=$(sha256sum "$TMP/$ZIP" | awk '{print $1}')
+if [ "$ACTUAL_SHA256" != "$SHA256" ]; then
+    echo "Hash mismatch, file may be corrupted!"
+    exit 1
+fi
+echo "Hash verified successfully."
+
+echo "Unzipping $ZIP ..."
+unzip -q "$TMP/$ZIP" -d "$TMP/$FILE"
+
+echo "Copying files ..."
+cp -R "$TMP/$FILE/." "$TARGET_DIR/"
+
+echo "Script finished successfully."
