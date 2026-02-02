b3b66c2ec9804ba1624ec6849c69abca2d92b262
Corvin Lasogga <c.lasogga@gmail.com>
2026-02-02 19:07:00 +0100

    fix(writers/pdf): Fix download script of flag_icons to verify by sha256

--- src/travelpost/writers/pdf/libs/flag_icons/download.sh
+++ src/travelpost/writers/pdf/libs/flag_icons/download.sh
@@ -2,7 +2,10 @@
 set -e
 
 VERSION="7.5.0"
-URL="https://github.com/lipis/flag-icons/archive/refs/tags/v$VERSION.zip"
+FILE="v${VERSION}"
+ZIP="${FILE}.zip"
+URL="https://github.com/lipis/flag-icons/archive/refs/tags/${ZIP}"
+SHA256="7eafedfc07e16ce640dd53309fbe147be0dcfd36d027646cf3798aea79a18913"
 TARGET_DIR="lib/flag-icons"
 
 TMP="$(mktemp -d)"
@@ -10,8 +13,22 @@ trap 'rm -rf "$TMP"' EXIT
 
 mkdir -p "$TARGET_DIR"
 
-curl -L "$URL" -o "$TMP/fi.zip"
-unzip -q "$TMP/fi.zip" -d "$TMP"
+echo "Downloading $ZIP ..."
+curl -L "$URL" -o "$TMP/$ZIP"
 
-cp -R "$TMP/flag-icons-$VERSION/flags/." "$TARGET_DIR/flags/"
-cp "$TMP/flag-icons-$VERSION/country.json" "$TARGET_DIR/country.json"
+echo "Verifying hash ..."
+ACTUAL_SHA256=$(sha256sum "$TMP/$ZIP" | awk '{print $1}')
+if [ "$ACTUAL_SHA256" != "$SHA256" ]; then
+    echo "Hash mismatch, file may be corrupted!"
+    exit 1
+fi
+echo "Hash verified successfully."
+
+echo "Unzipping $ZIP ..."
+unzip -q "$TMP/$ZIP" -d "$TMP/$FILE"
+
+echo "Copying files ..."
+cp -R "$TMP/$FILE/flag-icons-$VERSION/flags/." "$TARGET_DIR/flags/"
+cp "$TMP/$FILE/flag-icons-$VERSION/country.json" "$TARGET_DIR/country.json"
+
+echo "Script finished successfully."
