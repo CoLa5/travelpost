c3a57410718a7e1d025a2364a61852923e75049a
Corvin Lasogga <c.lasogga@gmail.com>
2026-02-10 09:52:59 +0100

    fix(writers/pdf): Fix logging of multiBuild

--- src/travelpost/writers/pdf/libs/reportlab/platypus/doc_template.py
+++ src/travelpost/writers/pdf/libs/reportlab/platypus/doc_template.py
@@ -2,10 +2,13 @@
 
 from collections.abc import Sequence
 import enum
+import logging
 import pathlib
 from typing import Any, Self
 
+from reportlab.pdfgen.canvas import Canvas
 from reportlab.platypus import BaseDocTemplate
+from reportlab.platypus import Flowable
 from reportlab.rl_config import defaultPageSize
 
 from travelpost.writers.pdf.libs.reportlab.libs import Box
@@ -16,6 +19,8 @@ from travelpost.writers.pdf.libs.reportlab.platypus.page_abc import (
     PageTemplateABC,
 )
 from travelpost.writers.pdf.libs.reportlab.settings import SHOW_BOUNDARY
+
+logger = logging.getLogger("reportlab.platypus")
 
 
 class VarLifetime(enum.StrEnum):
@@ -88,3 +93,77 @@ class DocTemplate(PageABC, BaseDocTemplate):
     def _calc(self) -> None:
         # NOTE: Overwritten by `PageABC`-properties
         pass
+
+    def multiBuild(
+        self,
+        flowables: Sequence[Flowable],
+        canvasmaker: type[Canvas] = Canvas,
+        filename: pathlib.Path | str | None = None,
+        max_passes: int = 10,
+    ) -> int:
+        """Builds the document from a list of flowables.
+
+        In case of indexing flowables like :py:cls:`TableOfContents` or
+        :py:cls:`Index`, it makes up `max_passes` to satisfy the indexing
+        flowables.
+
+        Args:
+            flowables: The list of flowables.
+            canvasmaker: The canvasmaker to build the document. Defaults to
+                :py:cls:`Canvas`.
+            filename: If the filename is provided, this filename is used instead
+                of the one provided upon initialization. Defauts to ``None``.
+            max_passes: The number of allowed build passes. Defaults to ``10``.
+
+        Returns:
+            The number of passes.
+        """
+        self._indexingFlowables = []
+        for flow in flowables:
+            if flow.isIndexing():
+                self._indexingFlowables.append(flow)
+
+        # better fix for filename is a 'file' problem
+        self._doSave = 0
+        happy = False
+        passes = 0
+        mbe = []
+        self._multiBuildEdits = mbe.append
+        while not happy and passes < max_passes:
+            passes += 1
+            if self._onProgress:
+                self._onProgress("PASS", passes)
+            if self._debug:
+                logger.debug("Building pass %d ...", passes)
+
+            for flow in self._indexingFlowables:
+                flow.beforeBuild()
+
+            # Work with a copy of the story, since it is consumed
+            temp_flowables = flowables[:]
+            super().build(
+                temp_flowables, filename=filename, canvasmaker=canvasmaker
+            )
+
+            for flow in self._indexingFlowables:
+                flow.afterBuild()
+
+            happy = self._allSatisfied()
+            if happy:
+                self._doSave = 0
+                self.canv.save()
+                if self._debug:
+                    logger.debug("Saved after %d passes", passes)
+                break
+
+            # Work through any edits
+            while mbe:
+                e = mbe.pop(0)
+                e[0](*e[1:])
+
+        del self._multiBuildEdits
+
+        if not happy and passes >= max_passes:
+            msg = f"Index entries not resolved after {max_passes:d} passes"
+            raise IndexError(msg)
+        return passes
