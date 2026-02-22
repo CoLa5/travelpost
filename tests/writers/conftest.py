79d03afe17e6448e1514744f6ec385f1c53c4a47
Corvin Lasogga <c.lasogga@gmail.com>
2026-02-22 13:25:08 +0100

    test(writers/map): Add tests of poi and include manual browser tests that are excluded by default

--- tests/writers/conftest.py
+++ tests/writers/conftest.py
@@ -20,6 +20,29 @@ def pytest_addoption(parser: pytest.Parser):
         default="auto",
         help="Set mocking mode: auto, mock, or no-mock",
     )
+    parser.addoption(
+        "--manual",
+        action="store_true",
+        default=False,
+    )
+
+
+def pytest_collection_modifyitems(
+    config: pytest.Config,
+    items: list[pytest.Item],
+) -> None:
+    if config.getoption("--manual"):
+        return
+    for item in items:
+        if "manual" in item.keywords:
+            item.add_marker(pytest.mark.skip(reason="need --run-special"))
+
+
+def pytest_configure(config: pytest.Config) -> None:
+    config.addinivalue_line(
+        "markers",
+        "manual: mark test to run only when called manually",
+    )
 
 
 @pytest.fixture(scope="session")
--- tests/writers/map_test.py
+++ tests/writers/map_test.py
@@ -1,4 +1,10 @@
-"""PS Tests."""
+"""Map Tests.
+
+NOTE: To run `test_map_in_browser` execute:
+    ```shell
+    $ uv run pytest .\tests\writers\map_test.py::test_map_in_browser --manual
+    ```
+"""
 
 import pathlib
 from typing import Any
@@ -10,6 +16,48 @@ from tests.writers import IMG_PATH
 from tests.writers import OUT_PATH
 from travelpost.writers import map
 
+MAP_KWARGS: dict[str, Any] = dict(
+    points=[
+        map.Point(lat=48.864716, lon=13.404954, transport="flight"),
+        map.Point(lat=52.520008, lon=13.404954, transport="flight"),
+        map.Point(lat=48.864716, lon=2.349014, transport="car"),
+        map.Point(lat=51.509865, lon=-0.118092, transport="walking"),
+    ],
+    posts=[
+        map.Post(
+            name="Berlin",
+            lat=52.520008,
+            lon=13.404954,
+            image_path=IMG_PATH,
+        ),
+        map.Post(
+            name="Paris",
+            lat=48.864716,
+            lon=2.349014,
+            image_path=None,
+        ),
+    ],
+    pois=[
+        map.PointOfInterest(
+            name="Mont Blanc\n4808",
+            lat=45.83278,
+            lon=6.86500,
+            symbol="mountain",
+        ),
+        map.PointOfInterest(
+            name="Amsterdam",
+            lat=52.3709,
+            lon=4.9016,
+            symbol="city",
+        ),
+        map.PointOfInterest(
+            lat=47.5833,
+            lon=9.3833,
+            symbol="lake",
+        ),
+    ],
+)
+
 
 def create_map(
     bounds: map.Bounds | None = None,
@@ -17,26 +65,7 @@ def create_map(
     styles: map.Styles | None = None,
 ) -> map.Map:
     return map.Map(
-        points=[
-            map.Point(lat=48.864716, lon=13.404954, transport="flight"),
-            map.Point(lat=52.520008, lon=13.404954, transport="flight"),
-            map.Point(lat=48.864716, lon=2.349014, transport="car"),
-            map.Point(lat=51.509865, lon=-0.118092, transport="walking"),
-        ],
-        posts=[
-            map.Post(
-                name="Berlin",
-                lat=52.520008,
-                lon=13.404954,
-                image_path=IMG_PATH,
-            ),
-            map.Post(
-                name="Paris",
-                lat=48.864716,
-                lon=2.349014,
-                image_path=None,
-            ),
-        ],
+        **MAP_KWARGS,
         bounds=bounds,
         show_only_flight_icons=show_only_flight_icons,
         styles=styles,
@@ -47,26 +76,7 @@ def create_print_map(
     bounds: map.Bounds | None = None,
 ) -> map.PrintMap:
     return map.PrintMap(
-        points=[
-            map.Point(lat=48.864716, lon=13.404954, transport="flight"),
-            map.Point(lat=52.520008, lon=13.404954, transport="flight"),
-            map.Point(lat=48.864716, lon=2.349014, transport="car"),
-            map.Point(lat=51.509865, lon=-0.118092, transport="walking"),
-        ],
-        posts=[
-            map.Post(
-                name="Berlin",
-                lat=52.520008,
-                lon=13.404954,
-                image_path=IMG_PATH,
-            ),
-            map.Post(
-                name="Paris",
-                lat=48.864716,
-                lon=2.349014,
-                image_path=None,
-            ),
-        ],
+        **MAP_KWARGS,
         bounds=bounds,
     )
 
@@ -78,6 +88,12 @@ def read_png_info(png_path: pathlib.Path) -> tuple[float, float, int]:
         assert dpi_tuple[0] == dpi_tuple[1]
         dpi = int(dpi_tuple[0]) if dpi_tuple else None
         return width, height, dpi
+
+
+@pytest.mark.manual
+def test_map_in_browser() -> None:
+    m = create_map()
+    m.show_in_browser()
 
 
 @pytest.mark.parametrize(
@@ -132,6 +148,12 @@ def test_map_png(i: int, kwargs: dict[str, Any]) -> None:
     assert w == kwargs.get("width", 1920)
     assert h == kwargs.get("height", 1080)
     assert abs(dpi - kwargs.get("dpi", 300)) <= 1  # rounding error bof png
+
+
+@pytest.mark.manual
+def test_print_map_in_browser() -> None:
+    m = create_print_map()
+    m.show_in_browser()
 
 
 @pytest.mark.parametrize(
