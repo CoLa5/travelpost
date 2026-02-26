1ea7d2690a857a99efd1b1d017bef1293438dddc
Corvin Lasogga <c.lasogga@gmail.com>
2026-02-26 15:23:16 +0100

    feat(writers/pdf): Add abstract page

---
+++ src/travelpost/writers/fpdf/pages/abc.py
@@ -0,0 +1,29 @@
+"""Abstract classes."""
+
+import abc
+from typing import TYPE_CHECKING
+
+import fpdf
+
+from travelpost.writers.fpdf.lib import Stylesheet
+
+
+class PageABC(abc.ABC):
+    """Abstract Page."""
+
+    if TYPE_CHECKING:
+        styles: Stylesheet
+
+    def __init__(self, pdf: fpdf.FPDF, title: str) -> None:
+        self.pdf = pdf
+        self.title = title
+
+    def add_to_outline(self, *, heading: bool = True) -> None:
+        y = self.pdf.y
+        self.pdf.y = 0
+        self.pdf.start_section(self.title, level=0)
+        self.pdf.y = y
+        self.pdf.heading = self.title if heading else None
+
+    @abc.abstractmethod
+    def render(self) -> None: ...
