3b670f65b01657a58f0e1d639f8e8e238ec25d88
Corvin Lasogga <c.lasogga@gmail.com>
2026-03-05 10:30:15 +0100

    feat(writers/pdf): Improve draw/fill/stroke context of style

--- src/travelpost/writers/fpdf/lib/drawing.py
+++ src/travelpost/writers/fpdf/lib/drawing.py
@@ -1,8 +1,66 @@
 """Drawing."""
 
-from typing import Any
+from __future__ import annotations
+
+from collections.abc import Sequence
+from typing import Any, NamedTuple, Self
 
 import fpdf.drawing
+from fpdf.util import Number
+
+
+class _DashPattern(NamedTuple):
+    dash: float
+    gap: float
+    phase: float
+
+
+class DashPattern(_DashPattern):
+    """Dash Pattern `(dash, gap, phase)`."""
+
+    def __new__(
+        cls,
+        *args: DashPattern | Sequence[Number] | dict[str, Number] | Number,
+        dash: Number | None = None,
+        gap: Number | None = None,
+        phase: Number | None = None,
+    ) -> Self:
+        if len(args) == 0:
+            if any(
+                not isinstance(x, (float | int)) for x in (dash, gap, phase)
+            ):
+                msg = "when using keywords all three values must be given"
+                raise ValueError(msg)
+            return super().__new__(
+                cls,
+                dash=float(dash),
+                gap=float(gap),
+                phase=float(phase),
+            )
+        if len(args) == 1:
+            if isinstance(args[0], cls):
+                return super().__new__(
+                    cls, dash=args[0].dash, gap=args[0].gap, phase=args[0].phase
+                )
+            if isinstance(args[0], Sequence):
+                return cls(*args[0])
+            if isinstance(args[0], dict):
+                return super().__new__(
+                    cls,
+                    dash=args[0].get("dash", 0.0),
+                    gap=args[0].get("gap", 0.0),
+                    phase=args[0].get("phase", 0.0),
+                )
+            if isinstance(args[0], Number):
+                v = float(args[0])
+                return super().__new__(cls, dash=v, gap=v, phase=v)
+            msg = f"invalid type: {type(args[0]).__name__:s}"
+            raise TypeError(msg)
+        if len(args) == 2:
+            return super().__new__(cls, *args, 0)
+        if len(args) == 3:
+            return super().__new__(cls, *args)
+        return super().__new__(cls, *args)
 
 
 def convert_style_attrib(
@@ -18,3 +76,11 @@ def convert_style_attrib(
             ...,
         }:
             setattr(group.style, key, val)
+
+
+def to_dash_pattern_or_none(
+    dp: DashPattern | Sequence[Number] | dict[str, Number] | Number | None,
+) -> DashPattern | None:
+    if dp is None:
+        return None
+    return DashPattern(dp)
--- src/travelpost/writers/fpdf/lib/style.py
+++ src/travelpost/writers/fpdf/lib/style.py
@@ -8,15 +8,17 @@ from typing import Any, TYPE_CHECKING
 
 import fpdf
 from fpdf.drawing_primitives import DeviceGray
+from fpdf.util import FloatTolerance
 from fpdf.util import Padding
 
 from travelpost.writers.fpdf.lib.color import ColorT
 from travelpost.writers.fpdf.lib.color import DeviceColor
 from travelpost.writers.fpdf.lib.color import to_color
 from travelpost.writers.fpdf.lib.color import to_color_or_none
+from travelpost.writers.fpdf.lib.drawing import DashPattern
+from travelpost.writers.fpdf.lib.drawing import to_dash_pattern_or_none
 from travelpost.writers.fpdf.lib.enums import TextTransform
 from travelpost.writers.fpdf.lib.fonts import FontFace
-from travelpost.writers.fpdf.lib.fonts import TextStyle
 
 
 class Style(Mapping[str, Any]):
@@ -29,7 +31,9 @@ class Style(Mapping[str, Any]):
         leading: float
         align: fpdf.Align
         border_radius: float
+        center: bool
         color: ColorT
+        dash_pattern: DashPattern | None
         fill_color: ColorT | None
         margin_bottom: float
         margin_left: float
@@ -52,7 +56,9 @@ class Style(Mapping[str, Any]):
         "leading": (float, 1.0),
         "align": (fpdf.Align.coerce, fpdf.Align.L),
         "border_radius": (float, 0),
+        "center": (bool, False),
         "color": (to_color, DeviceGray(0)),
+        "dash_pattern": (to_dash_pattern_or_none, None),
         "fill_color": (to_color_or_none, None),
         "margin_bottom": (float, 0),
         "margin_left": (float, 0),
@@ -119,6 +125,27 @@ class Style(Mapping[str, Any]):
         return self.to_str()
 
     @property
+    def draw_context(self) -> dict[str, Any]:
+        ctx = self.fill_context
+        ctx.update(self.stroke_context)
+        return ctx
+
+    @property
+    def fill_context(self) -> dict[str, Any]:
+        if self.fill_color is None:
+            return {}
+
+        ctx = {}
+        for key, type_ in (
+            ("fill_color", DeviceColor.__value__),
+            ("fill_opacity", float),
+        ):
+            val = getattr(self, key)
+            if isinstance(val, type_):
+                ctx[key] = val
+        return ctx
+
+    @property
     def fill_opacity(self) -> float | None:
         if self.fill_color is None:
             return None
@@ -150,6 +177,39 @@ class Style(Mapping[str, Any]):
                 for k in {"top", "right", "bottom", "left"}
             }
         )
+
+    @property
+    def render_style(self) -> fpdf.enums.RenderStyle | None:
+        fill = self.fill_color is not None
+        draw = self.stroke_color is not None and FloatTolerance.greater_than(
+            self.stroke_width, 0
+        )
+        if fill and draw:
+            return fpdf.enums.RenderStyle.DF
+        if draw:
+            return fpdf.enums.RenderStyle.D
+        if fill:
+            return fpdf.enums.RenderStyle.F
+        return None
+
+    @property
+    def stroke_context(self) -> dict[str, Any]:
+        if self.stroke_color is None or self.stroke_width <= 0:
+            return {}
+
+        ctx = {}
+        for fpdf_key, style_key, type_ in (
+            ("draw_color", "stroke_color", DeviceColor.__value__),
+            ("stroke_opacity", None, float),
+            ("line_width", "stroke_width", float),
+            ("stroke_cap_style", None, fpdf.enums.StrokeCapStyle),
+            ("stroke_join_style", None, fpdf.enums.StrokeJoinStyle),
+            ("dash_pattern", None, DashPattern),
+        ):
+            val = getattr(self, style_key or fpdf_key)
+            if isinstance(val, type_):
+                ctx[fpdf_key] = val
+        return ctx
 
     @property
     def stroke_opacity(self) -> float | None:
@@ -232,25 +292,48 @@ class Stylesheet(Mapping[str, Style]):
 
 
 class StyledPDF:
+    """Styled PDF.
+
+    Uses :py:cls:`Style` for `cell`, `multi_cell` and `write`.
+    """
+
     def __init__(self, pdf: fpdf.FPDF, style: Style) -> None:
         self.pdf = pdf
         self.style = style
+
+    @contextlib.contextmanager
+    def pad(
+        self,
+        new_x: fpdf.XPos | str = fpdf.XPos.RIGHT,
+        new_y: fpdf.YPos | str = fpdf.YPos.NEXT,
+    ) -> Iterator[None]:
+        self.pdf.x += self.style.padding.left
+        self.pdf.y += self.style.padding_top
+        try:
+            yield
+        finally:
+            match fpdf.XPos.coerce(new_x):
+                case fpdf.XPos.LEFT:
+                    self.pdf.x -= self.style.padding.left
+                case fpdf.XPos.RIGHT:
+                    self.pdf.x += self.style.padding.right
+            match fpdf.YPos.coerce(new_y):
+                case fpdf.YPos.TOP | fpdf.YPos.LAST:
+                    self.pdf.y -= self.style.padding.top
+                case fpdf.YPos.NEXT:
+                    self.pdf.y += self.style.padding.bottom
 
     def cell(
         self,
         text: str,
         *,
         border: bool = False,
-        center: bool | None = None,
         h: float | None = None,
         w: float | None = None,
         link: str | int = "",
         new_x: fpdf.XPos | str = fpdf.XPos.RIGHT,
         new_y: fpdf.YPos | str = fpdf.YPos.TOP,
     ) -> bool:
-        if center is None:
-            center = self.style.align == fpdf.Align.C
-
         text = self.style.text_transform.transform(text)
         w = w or self.pdf.get_cell_width(text)
         h = h or self.style.line_height
@@ -258,112 +341,117 @@ class StyledPDF:
         pad_h = self.style.padding.top + h + self.style.padding.bottom
         pad_w = self.style.padding.left + w + self.style.padding.right
 
-        if self.style.fill_color is not None:
-            with self.pdf.local_context(fill_opacity=self.style.fill_color.a):
-                if center is True:
-                    self.pdf.set_x_by_align(self.style.align, pad_w)
+        self.fill(pad_w, pad_h)
+
+        if border:
+            if self.style.center is True:
+                self.pdf.set_x_by_align(self.style.align, pad_w)
+            self.pdf.rect(self.pdf.x, self.pdf.y, pad_w, pad_h, style="D")
+
+        with self.pad(new_x=new_x, new_y=new_y):
+            return self.pdf.cell(
+                w,
+                h=h,
+                text=text,
+                align=self.style.align,
+                border=border,
+                center=self.style.center,
+                link=link,
+                new_x=new_x,
+                new_y=new_y,
+            )
+
+    def fill(self, w: float, h: float) -> None:
+        if self.style.render_style is not None:
+            with self.pdf.local_context(**self.style.draw_context):
+                if self.style.center is True:
+                    self.pdf.set_x_by_align(self.style.align, w)
                 # NOTE: in rect if r > min(w,h): r /=min(w,h)
                 r = self.style.border_radius
-                if r > min(pad_w, pad_h) / 2:
-                    r = min(pad_w, pad_h) / 2
+                if FloatTolerance.greater_equal(r, min(w, h) / 2):
+                    r = min(w, h) / 2 * (1 - FloatTolerance.TOLERANCE)
                 self.pdf.rect(
                     self.pdf.x,
                     self.pdf.y,
-                    pad_w,
-                    pad_h,
-                    style="F",
+                    w,
+                    h,
+                    style=self.style.render_style,
                     round_corners=r != 0,
                     corner_radius=r,
                 )
 
-        if border:
-            if center is True:
-                self.pdf.set_x_by_align(self.style.align, pad_w)
-            self.pdf.rect(self.pdf.x, self.pdf.y, pad_w, pad_h, style="D")
-
-        self.pdf.x += self.style.padding.left
-        self.pdf.y += self.style.padding_top
-
-        res = self.pdf.cell(
-            w,
-            h=h,
-            text=text,
-            align=self.style.align,
-            border=border,
-            center=center,
-            link=link,
-            new_x=new_x,
-            new_y=new_y,
-        )
-        match fpdf.XPos.coerce(new_x):
-            case fpdf.XPos.LEFT:
-                self.pdf.x -= self.style.padding.left
-            case fpdf.XPos.RIGHT:
-                self.pdf.x += self.style.padding.right
-        match fpdf.YPos.coerce(new_y):
-            case fpdf.YPos.TOP | fpdf.YPos.LAST:
-                self.pdf.y -= self.style.padding.top
-            case fpdf.YPos.NEXT:
-                self.pdf.y += self.style.padding.bottom
-        return res
+    def ln(self, h: float | None = None) -> None:
+        self.pdf.ln(h=h or self.style.line_height)
 
     def multi_cell(
         self,
         text: str,
         *,
         border: bool = False,
+        dry_run: bool = False,
+        link: str | int = "",
         new_x: fpdf.XPos | str = fpdf.XPos.RIGHT,
         new_y: fpdf.YPos | str = fpdf.YPos.NEXT,
-        w: float | None = None,
-    ) -> bool:
+        output: fpdf.enums.MethodReturnValue
+        | str = fpdf.enums.MethodReturnValue.PAGE_BREAK,
+        w: float = 0,
+    ) -> (
+        bool
+        | list[str]
+        | float
+        | tuple[bool, list[str]]
+        | tuple[bool, float]
+        | tuple[list[str], float]
+        | tuple[bool, list[str], float]
+    ):
         text = self.style.text_transform.transform(text)
-        w = w or max(self.pdf.get_cell_width(t) for t in text.split("\n"))
-        pad_w = self.style.padding.left + w + self.style.padding.right
+        if w == 0:
+            pad_w = self.pdf.w - self.pdf.r_margin - self.pdf.x
+            w = pad_w - self.style.padding.left - self.style.padding.right
+        else:
+            pad_w = self.style.padding.left + w + self.style.padding.right
 
-        if self.style.fill_color is not None:
-            with self.pdf.local_context(fill_opacity=self.style.fill_color.a):
+        # Support of border radius and fill opacity for fill
+        if not dry_run and self.style.render_style is not None:
+            if self.style.center is True:
                 self.pdf.set_x_by_align(self.style.align, pad_w)
-                n_lines = len(
-                    self.pdf.multi_cell(
-                        pad_w,
-                        h=self.style.line_height,
-                        text=text,
-                        align=self.style.align,
-                        dry_run=True,
-                        padding=self.style.padding,
-                        output=fpdf.enums.MethodReturnValue.LINES,
-                    )
-                )
-                pad_h = (
-                    self.style.padding.top
-                    + self.style.line_height * n_lines
-                    + self.style.padding.bottom
-                )
-
-                # NOTE: in rect if r > min(w,h): r /=min(w,h)
-                r = self.style.border_radius
-                if r > min(pad_w, pad_h) / 2:
-                    r = min(pad_w, pad_h) / 2
-                self.pdf.rect(
-                    self.pdf.x,
-                    self.pdf.y,
-                    pad_w,
-                    pad_h,
-                    style="F",
-                    round_corners=r != 0,
-                    corner_radius=r,
-                )
+            pad_h = self.pdf.multi_cell(
+                pad_w,
+                h=self.style.line_height,
+                text=text,
+                align=self.style.align,
+                center=self.style.center,
+                dry_run=True,
+                padding=self.style.padding,
+                output=fpdf.enums.MethodReturnValue.HEIGHT,
+            )
+            self.fill(pad_w, pad_h)
 
         return self.pdf.multi_cell(
             pad_w,
             h=self.style.line_height,
             text=text,
-            align=self.style.align,
             border=border,
-            center=self.style.align == fpdf.Align.C,
+            align=self.style.align,
+            center=self.style.center,
+            dry_run=dry_run,
+            link=link,
             new_x=new_x,
             new_y=new_y,
+            output=output,
             padding=self.style.padding,
+        )
+
+    def write(
+        self,
+        text: str,
+        *,
+        link: str = "",
+    ) -> bool:
+        return self.pdf.write(
+            h=self.style.line_height,
+            text=text,
+            link=link,
         )
 
 
